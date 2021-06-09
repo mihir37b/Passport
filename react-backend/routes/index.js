f0e7c3c80e2b1e207045448e0d836df30af63ccb
Mihir Bommakanti <mihir37b@gmail.com>
2021-06-09 18:55:34 -0400

    Connect Backend

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+//PORT=3001 npm start
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
